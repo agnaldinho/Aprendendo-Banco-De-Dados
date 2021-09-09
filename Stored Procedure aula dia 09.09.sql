acc9ffa2b3ff1c107bb13974f30cce6cadbded32
agnaldinho <73916890+agnaldinho@users.noreply.github.com>
2021-09-08 22:53:16 -0300

    Update Stored Procedure aula dia 09.09.sql

--- Stored Procedure aula dia 09.09.sql
+++ Stored Procedure aula dia 09.09.sql
@@ -11,3 +11,16 @@ DELIMITER ;
 call getallproducts(20);
 */
 
+DELIMITER //
+create PROCEDURE CountEmployees (
+	/*in total float ou */
+)
+begin
+	declare total int default 0;
+	select count(*) into total from employees;
+    select total;
+end //
+DELIMITER ;
+
+call CountEmployees(100);
+
